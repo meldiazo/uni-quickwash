2ac464fb90d58df05fda28e34bd21164c9281783
meldiazo <melaniediazog@gmail.com>
2026-09-14 17:37:43 -0400

    Fix stylesheet path for Render

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Univalle QuickWash' }}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
 <header class="site-header">
